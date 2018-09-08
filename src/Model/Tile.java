0cbf39d85b40de5963a534f8112050e257dfcfe0
Ben PH <b.pieters-hawke@unsw.edu.au>
2018-09-08 17:14:30 +1000

    Start implementing Tile tests

--- src/Model/Tile.java
+++ src/Model/Tile.java
@@ -1,5 +1,7 @@
 package Model;
 
+import java.util.Objects;
+
 public class Tile {
     enum TileType {
         INVINCIBLE_WALL,
@@ -17,4 +19,24 @@ public class Tile {
         this.type = aType;
     }
 
+    public TileType getType() {
+        return type;
+    }
+
+    public void setType(TileType type) {
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tile tile = (Tile) o;
+        return type == tile.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type);
+    }
 }
