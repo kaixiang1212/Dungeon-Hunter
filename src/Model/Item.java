248ed1c2b682b2e775ece5b47b94e85159c0dc59
RichardLUNSW <36937714+RichardLUNSW@users.noreply.github.com>
2018-09-14 15:16:00 +1000

    Update Item.java

--- src/Model/Item.java
+++ src/Model/Item.java
@@ -1,8 +1,11 @@
 package Model;
 
 //Anything that can be picked up
-public interface Item {
-	public boolean isPotion();
-	public boolean isWeapon();
-	public boolean isArrow();
+public abstract class Item {
+	private boolean stackable;
+	
+	public boolean isPotion() { return this instanceof Potion};
+	public boolean isMeleeWeapon() {return this instanceof MeleeWeapon};
+	public boolean isRangedWeapon() {return this instanceof RangedWeapon};
+	public boolean isStackable() { return this.stackable};
 }
