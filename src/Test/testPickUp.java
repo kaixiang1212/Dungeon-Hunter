8fa68dd869dc3ff0eb8873857e9066fc46d7c66a
garyliangyou <z5116167@student.unsw.edu.au>
2018-09-15 00:24:56 +1000

    Add files via upload

--- src/Test/testPickUp.java
+++ src/Test/testPickUp.java
@@ -2,11 +2,14 @@ package Test;
 
 import static org.junit.Assert.*;
 
+import java.awt.Point;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import itemDesign.Arrow;
 import itemDesign.ComputerAgent;
+import itemDesign.Dungeon;
 import itemDesign.Fist;
 import itemDesign.Hover;
 import itemDesign.Hunter;
@@ -14,6 +17,7 @@ import itemDesign.Invincibility;
 import itemDesign.MoveBehaviour;
 import itemDesign.Player;
 import itemDesign.Potion;
+import itemDesign.RangedWeapon;
 import itemDesign.Sword;
 import itemDesign.Treasure;
 
@@ -58,7 +62,7 @@ public class testPickUp {
 		player.pickup(invi);
 		ComputerAgent ca = new Hunter();
 		assert(ca.getHealth() == 100);
-		player.attack(ca);
+		ca.attack(player);
 		assert(ca.getHealth() <= 0);
 		assert(player.getHealth() == 100);
 	}
@@ -67,27 +71,45 @@ public class testPickUp {
 	@Test
 	public void testHasSword() {
 		Player player = new Player();
-		assert(player.getMeleeWeapon().isFist());
+		assert(player.getInventory().isEmpty());
 		player.pickup(new Sword());
-		assertTrue(player.getMeleeWeapon().isSword());
+		assertTrue(player.getEquipped() instanceof Sword);
 	}
 	
 	@Test
 	public void testHasArrow() {
 		Player player = new Player();
-		assert(player.getRangedWeapon().getUses() == 0);
+		assert(player.getInventory().isEmpty());
 		player.pickup(new Arrow());
-		assertTrue(player.getRangedWeapon().getUses() == 1);
+		assertTrue(player.getEquipped() instanceof Arrow);
+		assertTrue(((RangedWeapon) player.getEquipped()).getUses() == 1);
+		player.pickup(new Arrow());
+		assertTrue(((RangedWeapon) player.getEquipped()).getUses() == 2);
+	}
+	
+	@Test
+	public void testMoveThroughInventory() {
+		Player player = new Player();
+		player.pickup(new Arrow());
+		assertTrue(player.getEquipped() instanceof Arrow);
+		player.pickup(new Sword());
+		player.getInventory().moveThrough();
+		assertTrue(player.getEquipped() instanceof Sword);
 	}
 	
 	@Test
 	public void testSwordAttack() {
 		Player player = new Player();
 		ComputerAgent ca = new Hunter();
+		Dungeon dungeon = new Dungeon(3);
+		Point pPos = new Point(1,1);
+		Point aPos = new Point(2,1);
 		player.pickup(new Sword());
+		dungeon.placeComputerAgent(ca, aPos);
+		dungeon.placePlayer(player, pPos);
 		assert(ca.getHealth() == 100);
 		assert(player.getHealth() == 100);
-		player.attack(ca);
+		player.use(dungeon);
 		assert(ca.getHealth() <= 0);
 		assert(player.getHealth() == 100);
 	}
@@ -97,10 +119,16 @@ public class testPickUp {
 	public void testArrowAttack() {
 		Player player = new Player();
 		ComputerAgent ca = new Hunter();
+		Dungeon dungeon = new Dungeon(4);
+		Point pPos = new Point(1,1);
+		Point aPos = new Point(3,1);
 		player.pickup(new Arrow());
+		dungeon.placeComputerAgent(ca, aPos);
+		dungeon.placePlayer(player, pPos);
 		assert(ca.getHealth() == 100);
 		assert(player.getHealth() == 100);
-		player.shoot();
+		player.use(dungeon);
+		System.out.println(ca.getHealth());
 		assert(ca.getHealth() <= 0);
 		assert(player.getHealth() == 100);
 	}
