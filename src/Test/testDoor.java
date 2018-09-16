ffc37cb2e7a1c146b3768795a9973704c6280917
Ben-PH <b.pieters-hawke@unsw.edu.au>
2018-09-16 19:51:44 +1000

    Move test-class global vars into tests and expand.
    It is absolutely crucial that tests do not run an
    assumption about the order in which they are run.
    
    Avoid doing this in the future.
    
    Also, Test scope was limited. Expanding the scope
    revealed that a hunter will move if their path
    is blocked in by a vertically aligned obsaticle
    and they have a valid move "up", but not so if
    horizontally aligned set of obstacles, and it
    has a valid move "left".

--- src/Test/testDoor.java
+++ src/Test/testDoor.java
@@ -1,10 +1,10 @@
 package Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
 import java.awt.Point;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 
 import Model.ComputerAgent;
@@ -15,14 +15,14 @@ import Model.Key;
 import Model.Player;
 import Model.Tile.TileType;
 
-class testDoor {
-
-	Dungeon basic = new Dungeon(3);
-	Player player = new Player();
-	ComputerAgent enemy = new Hunter();
+public class testDoor {
 
 	@Test
 	public void testClosedDoorObstructPlayer() {
+		Dungeon basic = new Dungeon(3);
+		Player player = new Player();
+		ComputerAgent enemy = new Hunter();
+
 		basic.placePlayer(player, new Point(1, 1));
 		basic.placeDoorKey(new Point(2, 1), new Point(3, 3));
 		basic.updatePlayer("d");
@@ -30,17 +30,99 @@ class testDoor {
 	}
 	
 	@Test
-	public void testClosedDoorObstructEnemy() {
+	public void testClosedDoorObstructEnemyShortestPathHorizontal() {
+		Dungeon basic = new Dungeon(3);
+		Player player = new Player();
+		ComputerAgent enemy = new Hunter();
+
 		basic.placePlayer(player, new Point(1, 1));
+
+		//divide player and hunter with a closed door and walls.
 		basic.placeDoorKey(new Point(2, 1), new Point(3, 3));
+		basic.placeTile(TileType.DESTRUCTABLE_WALL, new Point(2, 2));
+		basic.placeTile(TileType.DESTRUCTABLE_WALL, new Point(2, 3));
 		basic.placeComputerAgent(enemy, new Point(3, 1));
+
+		assertFalse(basic.isValidMove(new Point(2, 1)));
 		basic.updateAgents();
-		System.out.println(basic.isValidMove(new Point(2, 1)));
-		assertEquals(new Point(3, 2), enemy.getPos());
+
+		assertEquals(new Point(3, 1), enemy.getPos());
 	}
-	
+
+	@Test
+	public void testClosedDoorObstructEnemyWithOptionHorizontal() {
+		Dungeon basic = new Dungeon(3);
+		Player player = new Player();
+		ComputerAgent enemy = new Hunter();
+
+		basic.placePlayer(player, new Point(1, 1));
+
+		//divide player and hunter with a closed door and walls.
+		basic.placeTile(TileType.DESTRUCTABLE_WALL, new Point(2, 1));
+		basic.placeDoorKey(new Point(2, 2), new Point(3, 3));
+		basic.placeTile(TileType.DESTRUCTABLE_WALL, new Point(2, 3));
+		basic.placeComputerAgent(enemy, new Point(3, 1));
+
+		assertFalse(basic.isValidMove(new Point(2, 1)));
+		assertFalse(basic.isValidMove(new Point(2, 2)));
+		assertTrue(basic.isValidMove(new Point(3, 1)));
+
+		basic.updateAgents();
+
+		assertEquals(new Point(3, 1), enemy.getPos());
+	}
+
+	@Test
+	public void testClosedDoorObstructEnemyShortestPathVert() {
+		Dungeon basic = new Dungeon(3);
+		Player player = new Player();
+		ComputerAgent enemy = new Hunter();
+
+		basic.placePlayer(player, new Point(1, 1));
+
+		//divide player and hunter with a closed door and walls.
+		basic.placeDoorKey(new Point(1, 2), new Point(3, 3));
+		basic.placeTile(TileType.DESTRUCTABLE_WALL, new Point(2, 2));
+		basic.placeTile(TileType.DESTRUCTABLE_WALL, new Point(3, 3));
+		basic.placeComputerAgent(enemy, new Point(1, 3));
+
+		assertFalse(basic.isValidMove(new Point(1, 2)));
+		basic.updateAgents();
+
+		assertEquals(new Point(1, 3), enemy.getPos());
+	}
+
+	@Test
+	public void testClosedDoorObstructEnemyWithOptionVertical() {
+		Dungeon basic = new Dungeon(3);
+		Player player = new Player();
+		ComputerAgent enemy = new Hunter();
+
+		basic.placePlayer(player, new Point(1, 1));
+
+		//divide player and hunter with a closed door and walls.
+		basic.placeTile(TileType.DESTRUCTABLE_WALL, new Point(1, 2));
+		basic.placeDoorKey(new Point(2, 2), new Point(3, 3));
+		basic.placeTile(TileType.DESTRUCTABLE_WALL, new Point(3, 2));
+		basic.placeComputerAgent(enemy, new Point(2, 3));
+
+		assertFalse(basic.isValidMove(new Point(1, 2)));
+		assertFalse(basic.isValidMove(new Point(2, 2)));
+		assertFalse(basic.isValidMove(new Point(3, 2)));
+		assertTrue(basic.isValidMove(new Point(1, 3)));
+
+		basic.updateAgents();
+
+		// Known test failure - likely source updateAgents.
+		assertEquals(new Point(1, 3), enemy.getPos());
+	}
+
 	@Test
 	public void testUnlockDoor() {
+		Dungeon basic = new Dungeon(3);
+		Player player = new Player();
+		ComputerAgent enemy = new Hunter();
+
 		basic.placePlayer(player, new Point(1, 1));
 		basic.placeDoorKey(new Point(2, 2), new Point(2, 1));
 
@@ -54,6 +136,9 @@ class testDoor {
 	
 	@Test
 	public void testMoveableAfterUnlock() {
+		Dungeon basic = new Dungeon(3);
+		Player player = new Player();
+
 		basic.placePlayer(player, new Point(1, 1));
 		basic.placeDoorKey(new Point(2, 2), new Point(2, 1));
 		assertFalse(basic.isValidMove(new Point(2, 2)));
