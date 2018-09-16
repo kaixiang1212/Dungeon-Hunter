f86e6b9120805bc3b90c39d448b2cb56d4a52b4a
Ben-PH <b.pieters-hawke@unsw.edu.au>
2018-09-16 18:18:08 +1000

    Adjust test to not fail, use junit4, and migrate Tile tests

---
+++ src/Test/testTile.java
@@ -0,0 +1,33 @@
+package Test;
+
+
+import Model.Tile;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class testTile {
+
+    @Test
+    void CanMakeTile() {
+        Tile test1 = new Tile(Tile.TileType.INVINCIBLE_WALL);
+        Assert.assertEquals(test1.getType(), Tile.TileType.INVINCIBLE_WALL);
+        test1 = new Tile(Tile.TileType.DESTRUCTABLE_WALL);
+        Assert.assertEquals(test1.getType(), Tile.TileType.DESTRUCTABLE_WALL);
+        test1 = new Tile(Tile.TileType.PIT);
+        Assert.assertEquals(test1.getType(), Tile.TileType.PIT);
+        test1 = new Tile(Tile.TileType.OPEN_DOOR);
+        Assert.assertEquals(test1.getType(), Tile.TileType.OPEN_DOOR);
+        test1 = new Tile(Tile.TileType.CLOSED_DOOR);
+        Assert.assertEquals(test1.getType(), Tile.TileType.CLOSED_DOOR);
+    }
+
+    @Test
+    void CanSetTile() {
+        Tile test1 = new Tile(Tile.TileType.INVINCIBLE_WALL);
+        Assert.assertEquals(test1.getType(), Tile.TileType.INVINCIBLE_WALL);
+        test1.setType(Tile.TileType.EXIT);
+        Assert.assertNotEquals(test1.getType(), Tile.TileType.INVINCIBLE_WALL);
+        Assert.assertNotEquals(test1.getType(), Tile.TileType.EXIT);
+    }
+
+}
